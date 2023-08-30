8bce657a175f97f7d0ff9ed03a9e2f1d3d235de6
Nicolai Willems <nicolai@pnorental.com>
2023-08-30 19:53:20 +0200

    feat: advance when reached point

--- 2023/src/stores/destination.ts
+++ 2023/src/stores/destination.ts
@@ -1,18 +1,24 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-const destinations : {[key: string]: number[][]} = {
+type Destination = {
+  info: string
+  coord : [number, number]
+}
+
+const destinations : {[key: string]: Destination[]} = {
   "morten": [
-    [55.83586804837251, 12.270102782277808], // For enden af vejen
-    [55.83636084800567, 12.270125653891546] // ved huset
+    {info: "Flot, næste!", coord: [55.83586804837251, 12.270102782277808]}, // For enden af vejen
+    {info: "Færdig!", coord: [55.83636084800567, 12.270125653891546]} // ved huset
   ],
   "nicolai": [
-    [55.842241038500816, 12.42550192368334], // Skolevej
-    [55.8426482781694, 12.42579507996865] // Huset
+    {info: "Så er vi igang", coord: [55.8426482781694, 12.42579507996865]}, // Huset
+    {info: "Flot, næste!", coord: [55.842241038500816, 12.42550192368334] }, // Skolevej
+    {info: "Færdig!", coord: [55.8426482781694, 12.42579507996865] } // Huset
   ],
   "pno": [
-    [55.67337596130846, 12.589015591505902],
-    [55.672343845053206, 12.588795131895393]
+    {info: "Flot, næste!", coord: [55.67337596130846, 12.589015591505902]},
+    {info: "Færdig!", coord: [55.672343845053206, 12.588795131895393]}
   ]
 }
 
@@ -27,17 +33,17 @@ export const useDestinationStore = defineStore('destination', () => {
 
   console.log("Pursuing route", route, destinations, destinations[route])
 
-  const destination_list : { visited: Date | undefined, pos: number[] }[] = [];
+  const destination_list : { visited: Date | undefined, dest: Destination }[] = [];
   for (let pos of destinations[route]) {
     destination_list.push({
       visited: undefined,
-      pos: pos
+      dest: pos
     })
   }
 
   const list = ref(destination_list)
   const index = ref(0)
-  const currentDestination = computed(() => list.value[index.value].pos)
+  const currentDestination = computed(() => list.value[index.value].dest.coord)
   function nextDestination() {
     list.value[index.value].visited = new Date()
 
--- 2023/src/views/HomeView.vue
+++ 2023/src/views/HomeView.vue
@@ -17,6 +17,7 @@ function direction_diff(cur: [number, number], dest: [number, number]){
       lat2 = dest[0],
       lon2 = dest[1]
 
+  // calculation from https://stackoverflow.com/questions/6016772/point-to-direction-of-a-gps-position
   const radians = Math.atan2(
     Math.sin(lon2-lon1)*Math.cos(lat2),
     Math.cos(lat1)*Math.sin(lat2)-Math.sin(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1)
@@ -54,10 +55,42 @@ var direction_to_move = computed(() => {
   return direction_diff(location.pos, dest)
 })
 
+var are_we_there = computed(() => {
+  const dest = destination.currentDestination
+  const cur = location.pos
+
+  var lat1 = cur[0],
+      lon1= cur[1],
+      lat2 = dest[0],
+      lon2 = dest[1]
+
+  const dist = Math.sqrt( Math.pow((lat2 - lat1), 2) + Math.pow( (lon2 - lon1), 2 ) ) * Math.pow(10, 4)
+
+  if (dist > 1) {
+    return false
+  }
+
+  return true;
+})
+
+function nextDestination(){
+  alert(destination.info)
+  destination.nextDestination()
+}
+
 </script>
 
 <template>
   <main>
-    Du skal lige gå lidt mere mod {{  direction_to_move }}
+    <div>Du skal lige gå lidt mere mod {{  direction_to_move }}</div>
+    <div> Are we there yet? {{  are_we_there ? "Yup" : "Nope" }} </div>
+
+    <div>
+      <button @click="nextDestination" v-if="are_we_there">
+        Gå videre
+      </button>
+    </div>
+
+    
   </main>
 </template>
